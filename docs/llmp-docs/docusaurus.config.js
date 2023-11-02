e12d511cc4d020d63b8dc5754444fd528a981104
elokow <lukas.kowejsza@gmail.com>
2023-11-02 18:10:44 +0100

    fix project BaseUrl

--- docs/llmp-docs/docusaurus.config.js
+++ docs/llmp-docs/docusaurus.config.js
@@ -12,17 +12,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://py-llmp.com',
+  url: 'https://llmp.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/llmp/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
